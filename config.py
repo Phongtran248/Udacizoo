3db0b77d439e7ca6420b5e183cc093dd7eedd669
Phong Tran <phongtran248@gmail.com>
2020-12-06 09:29:41 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or '[SQL_SERVER_GOES_HERE]'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or '[SQL_DATABASE_GOES_HERE]'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or '[SQL_USER_NAME_GOES_HERE]'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '[SQL_PASSWORD_GOES_HERE]'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'hottea248.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'database-west'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'hottea248'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'phongl@n2017'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or '[BLOB_ACCOUNT_GOES_HERE]'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '[BLOB_STORAGE_KEY_GOES_HERE]'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '[BLOB_CONTAINER_GOES_HERE]'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'hotte248sa.blob.core.windows.net'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'AmTZZUnYySLnOd+pVZJJsrevlPUQqoRO1tbyy+hddQeLXVowUAqeL1cHmoZs6+iyrS2TDPAnVguX1k6tAcEmpA=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
